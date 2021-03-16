771ccc3e589e0f3411dcc978fb37bb79fb472c6e
mounika <kamagiri.mounika@gmail.com>
2021-03-16 17:15:56 +0530

    modified Ex4

--- Example4.java
+++ Example4.java
@@ -1,41 +1,45 @@
 import java.util.*;
 import java.util.stream.*;
-import java.util.Iterator;
 
 public class Example4{
 	public static void main(String args[]){
 	Example4 app=new Example4();
 	List<String> list1=Stream.of("apple","orange","grapes").collect(Collectors.toList());
 	List<String> list2=Stream.of("melon","apple","mango").collect(Collectors.toList());
-	app.run(list1,list2);
+	String string[]=app.getResulted(list1,list2);
+	for(String element:string){
+		System.out.println(element);
+	}
 	}
 	
-	public void run(List<String> list1, List<String> list2) {
-		Iterator<String> iterator1 = list1.iterator();
-		Iterator<String> iterator2 = list2.iterator();
-		String arr[] = new String[list1.size() + list2.size()];
-		int count = 0;
-		while (iterator1.hasNext()) {
-			String element = iterator1.next();
-			if (element.startsWith("a") || element.startsWith("g")) {
-				iterator1.remove();
-				continue;
+	public String[]  getResulted(List<String> list3, List<String> list4) {
+		List<String> result=new ArrayList<>();
+		for(int i=0;i<list3.size();i++) {
+			String element1 = list3.get(i);
+			if (element1.charAt(0)=='a' || element1.charAt(0)=='g') {
+			continue;	
+				
 			}
-			arr[count++] = element;
-		}
-		while (iterator2.hasNext()) {
-			String element = iterator2.next();
-			if (element.endsWith("n") || element.endsWith("e")) {
-				iterator2.remove();
-				continue;
-			}
-			arr[count++] = element;
-		}
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] != null) {
-				System.out.println(arr[i]);
+			else{
+				result.add(element1);
 			}
 		}
+		for(int i=0;i<list4.size();i++) {
+			String element2 = list4.get(i);
+			if (element2.charAt(element2.length()-1)=='n' || element2.charAt(element2.length()-1)=='e') {
+			continue;		
+				
+			}
+			else{
+				result.add(element2);
+			}
+		}
+		String array[]=new String[result.size()];
+		for(int i=0;i<result.size();i++){
+			array[i]=result.get(i);
+		}
+		return array;
+		
 	}
 }
 
